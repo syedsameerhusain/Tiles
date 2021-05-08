f5f934b4d0aa7d3a94296b30fa1bcf2e5fa1101a
Syed Sameer Husain <husainsam432@gmail.com>
2021-05-08 16:40:05 +0530

    First Commit

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express');
+const connectDB = require('./config/db');
+
 const app = express();
 
+connectDB();
+app.use(express.json({ extended: false }));
+// app.get('/', (req, res, next) => {
+//   res.send('API is Running');
+// });
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profiles', require('./routes/api/profiles'));
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res, next) => {
-  res.send('API is Running');
-});
 app.listen(PORT, () => console.log(`Server port started on port no. ${PORT}`));
